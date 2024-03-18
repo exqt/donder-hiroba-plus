b6fb2f6523aa3ce08513fa4543e6afe2d88a7463
exqt <8168124+exqt@users.noreply.github.com>
2024-03-18 21:29:01 +0900

    refactor: DonforceItem component

---
+++ src/components/Popup/DonforceItemComponent.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import { getDonforceLevel } from '../../lib/donforce'
+  import type { DonforceItem, SongData, SongScoreDetail } from '../../types'
+  import { icons } from '../../assets'
+
+  export let item: DonforceItem
+  export let i: number
+  export let songData: SongData | undefined
+  export let detail: SongScoreDetail | undefined
+  export let link: string
+  export let color: string
+
+</script>
+
+{#if detail}
+<a href={`${link}`} target="_blank">
+  <div class="card" style={`background-color: ${color}`}>
+    <span> {item.title} (#{i + 1}) </span>
+    <div class="items">
+      {#if songData?.levels[item.difficulty]}
+        {@const level = getDonforceLevel(songData, item.difficulty)}
+        <span> ★ {level.toFixed(1)} </span>
+        <span class="divider"> | </span>
+        <span> 💪 {item.donforce.toFixed(3)} </span>
+        <span class="divider"> | </span>
+      {/if}
+      <img class="icon" src={icons.badges[detail.badge]} alt={'badge-' + detail.badge}/>
+      <img class="icon" src={icons.crowns[detail.crown]} alt={detail.crown}/>
+    </div>
+  </div>
+</a>
+{/if}
+
+<style>
+  .card {
+    padding: 4px 0px;
+    margin: 4px 4px;
+    border-radius: 8px;
+    color: white;
+  }
+
+  .items {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    padding: 4px 0px;
+  }
+
+  .divider {
+    margin: 0px 4px;
+  }
+
+  .icon {
+    height: 22px;
+  }
+</style>
--- src/popup/DonforceList.svelte
+++ src/popup/DonforceList.svelte
@@ -2,11 +2,11 @@
   import { type ExtensionStorage } from '../lib/storage'
   import { onMount } from 'svelte'
   import { SongDB } from '../lib/songDB'
-  import { getDonforceLevel, getDonforceTopK } from '../lib/donforce'
+  import { getDonforceTopK } from '../lib/donforce'
   import { DIFFICULTY_TO_INDEX, DIFFICULTY_COLORS, DONFORCE_NUMBER_OF_RECORDS } from '../constants'
-  import { icons } from '../assets'
-  import type { DonforceItem, SongScore } from '../types'
+  import type { DifficultyType, DonforceItem, SongScore } from '../types'
   import { getSongDetailLink } from '../lib/songs'
+  import DonforceItemComponent from '../components/Popup/DonforceItemComponent.svelte'
 
   export let storage: ExtensionStorage
   export let songDB: SongDB
@@ -15,18 +15,18 @@
   let items: DonforceItem[] = []
 
   let totalDonforce = 0
+  let diffs: DifficultyType[] = []
 
   onMount(async () => {
     songDB = await SongDB.getInstance()
     scores = storage.getAllScores()
 
-    const diffs = [storage.settings.preferringDifficulty ?? 'oni']
+    diffs = [storage.settings.preferringDifficulty ?? 'oni']
     if (diffs[0] === 'oni') diffs.push('oni_ura')
 
     items = getDonforceTopK(scores, songDB, diffs, DONFORCE_NUMBER_OF_RECORDS)
     totalDonforce = items.reduce((acc, item) => acc + item.donforce, 0) / DONFORCE_NUMBER_OF_RECORDS
   })
-
 </script>
 
 <div class="wrapper">
@@ -35,29 +35,12 @@
   </div>
 
   <div class="score-container">
-    {#each items as item, i}
+    {#each items as item, i (item.songNo + item.difficulty)}
       {@const color = DIFFICULTY_COLORS[DIFFICULTY_TO_INDEX[item.difficulty]]}
       {@const songData = songDB.getSongData(item.songNo)}
       {@const detail = storage.getScoreByNo(item.songNo)?.details[item.difficulty]}
       {@const link = getSongDetailLink(item.songNo, item.difficulty)}
-      {#if detail}
-        <a href={`${link}`} target="_blank">
-          <div class="card" style={`background-color: ${color}`}>
-            <span> {item.title} (#{i + 1}) </span>
-            <div class="items">
-              {#if songData?.levels[item.difficulty]}
-                {@const level = getDonforceLevel(songData, item.difficulty)}
-                <span> ★ {level.toFixed(1)} </span>
-                <span class="divider"> | </span>
-                <span> 💪 {item.donforce.toFixed(3)} </span>
-                <span class="divider"> | </span>
-              {/if}
-              <img class="icon" src={icons.badges[detail.badge]} alt={'badge-' + detail.badge}/>
-              <img class="icon" src={icons.crowns[detail.crown]} alt={detail.crown}/>
-            </div>
-          </div>
-        </a>
-      {/if}
+      <DonforceItemComponent {item} {i} {songData} {detail} {link} {color} />
     {/each}
   </div>
 </div>
@@ -82,28 +65,5 @@
     flex-direction: column;
     flex-grow: 1;
     overflow: auto; /* Add this if you want .score-container to be scrollable when its content overflows */
-  }
-
-  .card {
-    padding: 4px 0px;
-    margin: 4px 4px;
-    border-radius: 8px;
-    color: white;
-  }
-
-  .items {
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    padding: 4px 0px;
-  }
-
-  .divider {
-    margin: 0px 4px;
-  }
-
-  .icon {
-    height: 22px;
   }
 </style>
