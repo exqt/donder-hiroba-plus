a5fb01594b22fcf43b866ec3728966927d02bae9
hotsixisbest <galactagun12@gmail.com>
2024-04-04 12:25:32 +0900

    getSongNoByTitle의 반환형을 string으로 수정

--- src/lib/songDB.ts
+++ src/lib/songDB.ts
@@ -28,11 +28,11 @@ export class SongDB {
     return this.songDataMap.get(songNo)
   }
 
-  getSongNoByTitle (title: string): number | undefined {
+  getSongNoByTitle (title: string): string | undefined {
     for (const songNo of this.songDataMap.keys()) {
       const song = this.songDataMap.get(songNo)
       if (song?.title === title) {
-        return parseInt(songNo)
+        return songNo
       }
     }
     return undefined
