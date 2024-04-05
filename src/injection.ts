5a26976a89e7c075a1df8c2a988876dbb72e0cd3
hotsixisbest <galactagun12@gmail.com>
2024-04-05 10:44:46 +0900

    hashRouter가 작동되도록 설정

--- src/injection.ts
+++ src/injection.ts
@@ -7,8 +7,11 @@ import score_detail from './injections/score_detail'
 import select_song from './injections/select_song'
 import i18n from './injections/i18n'
 import dani from './injections/dani'
+import hash, { hashChangeCallback } from './injections/hash'
 
 const runHiroba = async (): Promise<void> => {
+  window.addEventListener('hashchange', hashChangeCallback)
+
   const path = window.location.href.split('/').slice(3).join('/')
   const page = path.split('?')[0]
 
@@ -27,6 +30,10 @@ const runHiroba = async (): Promise<void> => {
 
   if (page === '') {
     await scriptMap['index.php']()
+    return
+  }
+  if (page.startsWith('#')) {
+    await hash()
     return
   }
 
