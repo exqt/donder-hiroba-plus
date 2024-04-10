faa13c07aa6f5aa56b24bf3172063723553c5f63
hotsixisbest <galactagun12@gmail.com>
2024-04-10 15:24:31 +0900

    fix: load score whenever visit /#/training

--- src/components/Training/pages/Training.svelte
+++ src/components/Training/pages/Training.svelte
@@ -3,7 +3,7 @@
     import TrainingCourseStorage from '../../../lib/trainingCourse'
     import { SongDB } from '../../../lib/songDB'
     import Course from '../view/Course.svelte'
-    import { link } from 'svelte-spa-router'
+    import { link, location } from 'svelte-spa-router'
 
     let reloading = false
     function reload (): void {
@@ -12,10 +12,18 @@
 </script>
 
 <a href="/training/add" use:link>추가</a>
-{#await Promise.all([TrainingCourseStorage.getInstance(), ScoreStorage.getInstance(), SongDB.getInstance()]) then [courseStorage, scoreStorage, songDB]}
-    {#key reloading}
-    {#each courseStorage.getAll() as course}
-        <Course {course} {scoreStorage} {songDB} {courseStorage} {reload}/>
-    {/each}
-    {/key}
-{/await}
+{#key $location}
+    {#await Promise.all([TrainingCourseStorage.getInstance(), ScoreStorage.getInstance(), SongDB.getInstance()]) then [courseStorage, scoreStorage, songDB]}
+        {#key reloading}
+            {#each courseStorage.getAll() as course}
+                <Course
+                    {course}
+                    {scoreStorage}
+                    {songDB}
+                    {courseStorage}
+                    {reload}
+                />
+            {/each}
+        {/key}
+    {/await}
+{/key}
