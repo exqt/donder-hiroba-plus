55a124f77a01327bf639e97f814a29465de13bec
HotsixMan <hotsixman05@gmail.com>
2024-10-22 21:08:32 +0900

    feat: 위키에 레이팅 데이터 전송 기능 추가

---
+++ public/rating.ts
@@ -0,0 +1,5 @@
+import RatingApp from '../src/components/Rating/RatingApp.svelte';
+
+new RatingApp({
+    target: document.querySelector('#app')
+})
---
+++ src/components/Rating/RatingProfile.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import type {CardData} from 'node-hiroba/types'
+
+    export let cardData: CardData;
+</script>
+
+<table>
+    <tr>
+        <td colspan="2">
+            <img src={cardData.myDon} alt="mydon" style="max-width: 100%;"/>
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Nickname
+        </td>
+        <td>
+            {cardData.nickname}
+        </td>
+    </tr>
+    <tr>
+        <td>
+            Taiko number
+        </td>
+        <td>
+            {cardData.taikoNumber}
+        </td>
+    </tr>
+</table>
+
+<style>
+    table{
+        max-width: 100%;border-collapse: collapse;
+    }
+
+    tr{
+        text-align: center;
+    }
+
+    td{
+        text-align: center;border: 1px solid black;
+    }
+</style>
