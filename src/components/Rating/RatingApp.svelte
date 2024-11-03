2296d453b18b514aa49cbd6d4a4ca664c81677c0
exqt <8168124+exqt@users.noreply.github.com>
2024-11-03 21:46:46 +0900

    refactor: rating upload page

--- src/components/Rating/RatingApp.svelte
+++ src/components/Rating/RatingApp.svelte
@@ -3,6 +3,8 @@
     import Profile from './RatingProfile.svelte'
     import type { CardData, ScoreData } from 'node-hiroba/types'
     import lzutf8 from 'lzutf8'
+    import type { ClearData } from './clearData'
+    import { waitFor } from '../../lib/utils'
 
     const wikiOrigin = 'https://taiko.wiki'
 
@@ -16,12 +18,77 @@
 
     // ready
     let sendType: 'clear' | 'score' = 'clear'
+
+    // 새로운 유틸리티 함수들
+    async function checkWikiLogin (): Promise<boolean> {
+      const wikiUser = await (await fetch(wikiOrigin + '/api/user', { credentials: 'include' })).json()
+      return wikiUser.logined === true
+    }
+
+    async function fetchScoreForSong (songNo: string, clearData: ClearData): Promise<ScoreData | null> {
+      const response: { songNo: string, body: { oni?: string, ura?: string } } =
+        { songNo, body: {} }
+
+      while (true) {
+        try {
+          if (clearData.difficulty.oni === undefined) break
+          response.body.oni = await (await fetch(
+            `https://donderhiroba.jp/score_detail.php?song_no=${songNo}&level=${4}`
+          )).text()
+          break
+        } catch {
+          console.log('fetching error try in 3 seconds')
+          await waitFor(3000)
+        }
+      }
+
+      while (true) {
+        try {
+          if (clearData.difficulty.ura === undefined) break
+          response.body.ura = await (await fetch(
+            `https://donderhiroba.jp/score_detail.php?song_no=${songNo}&level=${5}`
+          )).text()
+          break
+        } catch {
+          console.log('fetching error try in 3 seconds')
+          await waitFor(3000)
+        }
+      }
+
+      console.log(response)
+      if (response.body.oni === undefined && response.body.ura === undefined) return null
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      const parsed = parse.parseScoreData(response as any)
+      return parsed ?? null
+    }
+
+    async function uploadToWiki (donderData: CardData, clearData: ClearData[], scoreData: ScoreData[]): Promise<void> {
+      const data = JSON.stringify({
+        donderData,
+        clearData,
+        scoreData
+      })
+
+      console.log(data)
+
+      const compressedBody = lzutf8.compress(data, {
+        outputEncoding: 'ByteArray'
+      }) as Uint8Array
+
+      await fetch(wikiOrigin + '/api/user/donder', {
+        credentials: 'include',
+        method: 'POST',
+        body: compressedBody,
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    }
+
+    // 리팩토링된 send 함수
     async function send (cardData: CardData, sendType: 'clear' | 'score'): Promise<void> {
-      if (
-        !confirm(
-                `Send your donderhiroba datas to ${wikiOrigin}. It will be deleted together when you delete your account. Do you agree?`
-        )
-      ) {
+      if (!confirm(`Send your donderhiroba datas to ${wikiOrigin}. It will be deleted together when you delete your account. Do you agree?`)) {
         alert('Canceled.')
         message = ''
         uploadMessage = ''
@@ -29,12 +96,7 @@
         return
       }
 
-      const wikiUser = await (
-        await fetch(wikiOrigin + '/api/user', {
-          credentials: 'include'
-        })
-      ).json()
-      if (wikiUser.logined === false) {
+      if (!await checkWikiLogin()) {
         message = 'Wiki Not Logined'
         return
       }
@@ -42,144 +104,39 @@
       try {
         scene = 'upload'
         uploadMessage = ''
-        // 클리어 데이터만
+
+        uploadMessage = 'Fetching clear data...'
+        let clearData = await hiroba.getClearData(null)
+        clearData = clearData.filter((song) => song.songNo.length < 3) // TODO
+
         if (sendType === 'clear') {
-          uploadMessage = 'Fetching clear data...'
-          const clearData = await hiroba.getClearData(null)
           uploadMessage = 'Uploading clear data...'
-          const data = JSON.stringify({
-            donderData: cardData,
-            clearData
-          })
-          const body = lzutf8.compress(data, {
-            outputEncoding: 'ByteArray'
-          }) as Uint8Array
-          await fetch(wikiOrigin + '/api/user/donder', {
-            credentials: 'include',
-            method: 'POST',
-            body,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-        } else { // 점수 데이터
+          await uploadToWiki(cardData, clearData, [])
+        } else {
           uploadMessage = 'Updating score...'
-          await hiroba.updateScore(null)
-          uploadMessage = 'Fetching clear data...'
-          const clearData = await hiroba.getClearData(null)
-          const songNoDatas: Array<{ songNo: string, hasUra: boolean }> =
-                    []
-          clearData.forEach((e) => {
-            const hasUra = !(e.difficulty.ura === undefined)
-            songNoDatas.push({
-              songNo: e.songNo,
-              hasUra
-            })
-          })
+          // await hiroba.updateScore(null)
 
-          counts = clearData.length
-          complete = 0
-          uploadMessage = `Fetch score data... (0/${counts})`
-
-          const grouped: Array<
-          Array<{ songNo: string, hasUra: boolean }>
-          > = [[], [], [], [], [], [], [], [], [], []]
-          songNoDatas.forEach((song, index) => {
-            grouped[Number(index.toString().at(-1))].push(song)
-          })
-          const scoreData: Record<string, ScoreData> = {}
-          const errors: Array<{ songNo: string, hasUra: boolean }> = []
-          await Promise.all(
-            grouped.map(async (group) => {
-              for (const song of group) {
-                /** eslint-disable @typescript-eslint/no-unsafe-argument */
-                const response: any = {
-                  songNo: song.songNo,
-                  body: {}
-                }
-
-                try {
-                  const fetched = await fetch(
-                                    `https://donderhiroba.jp/score_detail.php?song_no=${song.songNo}&level=4`
-                  )
-                  const body = await fetched.text()
-                  response.body.oni = body
-                  if (song.hasUra) {
-                    const fetched = await fetch(
-                                        `https://donderhiroba.jp/score_detail.php?song_no=${song.songNo}&level=5`
-                    )
-                    const body = await fetched.text()
-                    response.body.ura = body
-                  }
-                } catch {
-                  errors.push(song)
-                  continue
-                }
-
-                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-                const parsed = parse.parseScoreData(response)
-
-                if (parsed !== null) {
-                  scoreData[song.songNo] = parsed
-                  complete++
-                  uploadMessage = `Fetch score data... (${complete}/${counts})`
-                }
-              }
-            })
-          )
-
-          for (const song of errors) {
-            const response: any = {
-              songNo: song.songNo,
-              body: {}
-            }
-
-            try {
-              const fetched = await fetch(
-                            `https://donderhiroba.jp/score_detail.php?song_no=${song.songNo}&level=4`
-              )
-              const body = await fetched.text()
-              response.body.oni = body
-              if (song.hasUra) {
-                const fetched = await fetch(
-                                `https://donderhiroba.jp/score_detail.php?song_no=${song.songNo}&level=5`
-                )
-                const body = await fetched.text()
-                response.body.ura = body
-              }
-            } catch {
-              errors.push(song)
-              continue
-            }
-
-            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-            const parsed = parse.parseScoreData(response)
-
-            if (parsed !== null) {
-              scoreData[song.songNo] = parsed
-              complete++
-              uploadMessage = `Fetch score data... (${complete}/${counts})`
+          const scoresToFetch: Array<Promise<ScoreData | null>> = []
+          for (const song of clearData) {
+            if (song.difficulty.oni !== undefined || song.difficulty.ura !== undefined) {
+              scoresToFetch.push(fetchScoreForSong(song.songNo, song))
             }
           }
 
-          const data = JSON.stringify({
-            donderData: cardData,
-            clearData,
-            scoreData
-          })
+          counts = scoresToFetch.length
+          complete = 0
+          uploadMessage = `Fetch score data... (0/${counts})`
 
-          const body = lzutf8.compress(data, {
-            outputEncoding: 'ByteArray'
-          }) as Uint8Array
+          const scoreData = await Promise.all(scoresToFetch.map(async (promise) => {
+            const result = await promise
+            complete++
+            uploadMessage = `Fetch score data... (${complete}/${counts})`
+            return result
+          }))
 
-          await fetch(wikiOrigin + '/api/user/donder', {
-            credentials: 'include',
-            method: 'POST',
-            body,
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
+          console.log(scoreData, clearData)
+
+          await uploadToWiki(cardData, clearData, scoreData.filter((score) => score !== null))
         }
 
         message = 'Upload completed'
---
+++ src/components/Rating/clearData.ts
@@ -0,0 +1,15 @@
+export type Difficulty = 'easy' | 'normal' | 'hard' | 'oni' | 'ura'
+
+export type Crown = 'played' | 'silver' | 'gold' | 'donderfull' | null
+export type Badge = 'rainbow' | 'purple' | 'pink' | 'gold' | 'silver' | 'bronze' | 'white' | null
+
+export interface Clear {
+  crown: Crown
+  badge: Badge
+}
+
+export interface ClearData {
+  title: string
+  songNo: string
+  difficulty: Partial<Record<Difficulty, Clear>>
+}
