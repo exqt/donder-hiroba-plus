e0f25d36000324cda3837658f16b359e5a7ce50a
exqt <8168124+exqt@users.noreply.github.com>
2025-06-29 19:31:52 +0900

    refactor: 레이팅 페이지 테이블 수정

--- src/components/Rating/ScoreDataTable.svelte
+++ src/components/Rating/ScoreDataTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { SortedScoreData } from './services/ScoreDataService'
-  import { DIFFICULTY_COLORS } from '../../constants'
+  import { icons } from '../../assets'
 
   export let scoreDataSorted: SortedScoreData[]
   export let totalPlayCount: string
@@ -24,15 +24,26 @@
   </button>
 
   {#if openPlayCount}
-    <span>Count: {scoreDataSorted.length}</span>
+    <span>Total Song Count: {scoreDataSorted.length}</span>
     <span>Total Play Count: {totalPlayCount}</span>
 
     <table class="play-count-table">
       <thead>
         <tr>
           <th>Name</th>
-          <th style="width: 60px">Diff</th>
-          <th style="width: 100px">Count</th>
+          <th style="width: 30px">Diff</th>
+          <th style="width: 30px">
+            <img src={icons.crowns.played} alt="Played" title="Played" />
+          </th>
+          <th style="width: 30px">
+            <img src={icons.crowns.silver} alt="Clear" title="Clear" />
+          </th>
+          <th style="width: 30px">
+            <img src={icons.crowns.gold} alt="Full Combo" title="Full Combo" />
+          </th>
+          <th style="width: 30px">
+            <img src={icons.crowns.donderfull} alt="Donder Full" title="Donder Full" />
+          </th>
         </tr>
       </thead>
       <tbody>
@@ -43,10 +54,15 @@
                 ({score.songNo}) {score.songName}
               </a>
             </td>
-            <td
-              style={`color: ${DIFFICULTY_COLORS[score.difficulty === 'oni' ? 3 : 4]}; font-weight: bold;`}
-            >{score.difficulty}</td>
-            <td>{score.score.count.play} / {score.score.count.clear} / {score.score.count.fullcombo} / {score.score.count.donderfullcombo}</td>
+            <td>
+                <img src={score.difficulty === 'oni' ? icons.oni : icons.ura} alt={score.difficulty} title={score.difficulty}
+                style="width: 30px; height: 30px; vertical-align: middle;"
+            />
+            </td>
+            <td>{score.score.count.play}</td>
+            <td>{score.score.count.clear}</td>
+            <td>{score.score.count.fullcombo}</td>
+            <td>{score.score.count.donderfullcombo}</td>
           </tr>
         {/each}
       </tbody>
@@ -76,5 +92,11 @@
     border: 1px solid black;
     padding: 5px;
     text-align: center;
+  }
+
+  .play-count-table th img {
+    width: 20px;
+    height: 20px;
+    vertical-align: middle;
   }
 </style>
